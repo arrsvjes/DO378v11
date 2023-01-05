380b6504ac4f7ef5044eb173862ed57833df3c7c
Student User <student@workstation.lab.example.com>
2023-01-05 14:56:31 -0500

    add solution custom with support swagger

---
+++ solutions/implement-review/microservice-speaker/src/main/java/org/acme/conference/speaker/SpeakerResource.java
@@ -0,0 +1,56 @@
+package org.acme.conference.speaker;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.annotations.jaxrs.QueryParam;
+
+@Path("/speaker")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SpeakerResource {
+    @Inject
+    SpeakerService service;
+
+    @GET
+    public List<Speaker> listAll() { 
+        return service.listAll();
+    }
+
+    @GET
+    @Path("/search")
+    public List<Speaker> search(@QueryParam("q") String q) { 
+        return service.search(q);
+    }
+    @GET
+    @Path("/{uuid}")
+    public Speaker findByUuid(@PathParam("uuid") UUID uuid) {
+                        return service.findByUuid(uuid)
+                            .orElseThrow(NotFoundException::new);
+    }
+    @POST
+    @Transactional
+    public Speaker create(Speaker newSpeaker) {
+        service.create(newSpeaker);
+        return newSpeaker;
+    }
+    
+    @DELETE
+    @Transactional
+    @Path("/{uuid}")
+    public void delete(@PathParam("uuid") UUID uuid) {
+        service.deleteByUuid(uuid);
+    }
+}
